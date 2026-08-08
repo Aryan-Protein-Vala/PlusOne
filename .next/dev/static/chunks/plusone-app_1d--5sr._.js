860e5eff0fc1e19af3cd0b438043e44c06c4fee0
Aryan <aryansharma24112003@gmail.com>
2026-08-08 15:03:16 +0530

    chore: clean up tracked .next folder from root and add gitignore

--- .next/dev/static/chunks/plusone-app_1d--5sr._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/plusone-app_src_app_globals_0tuvvzd.css","static/chunks/1m2e_next_dist_1d8-zkn._.js","static/chunks/1m2e_0cg6nfi._.js","static/chunks/plusone-app_src_17ir7ne._.js","static/chunks/plusone-app_1hp93-8._.js"],
-    source: "entry"
-});
--- .next/dev/static/chunks/plusone-app_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/plusone-app_src_app_globals_0tuvvzd.css","static/chunks/1m2e_next_dist_1d8-zkn._.js","static/chunks/plusone-app_src_components_landing_PlusOneLanding_tsx_17xu-32._.js","static/chunks/1m2e_1ir_4zb._.js"],
-    source: "entry"
-});
